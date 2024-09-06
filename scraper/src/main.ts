9b058fa4521757438e0e3eda73932951d3bad038
Aayushman Singh <aayushman2702@gmail.com>
2024-09-06 11:50:29 +0530

    kinda working insta scraper

---
+++ scraper/src/main.ts
@@ -0,0 +1,24 @@
+import { Actor } from 'apify'
+import { PlaywrightCrawler, log } from 'crawlee'
+import { router } from './route.js'
+
+await Actor.init()
+
+// This is better set with CRAWLEE_LOG_LEVEL env var
+// or a configuration option. This is just for show 😈
+log.setLevel(log.LEVELS.DEBUG)
+
+log.debug('Setting up crawler.')
+const crawler = new PlaywrightCrawler({
+  // I'm using a low maxRequestsPerCrawl so the
+  // crawler ends quicker for demo purposes
+  maxRequestsPerCrawl: 50,
+
+  // Instead of the long requestHandler with
+  // if clauses we provide a router instance.
+  requestHandler: router
+})
+
+await crawler.run(['https://www.instagram.com/explore/tags/japan'])
+
+await Actor.exit()
