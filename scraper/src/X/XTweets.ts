dff24415a7dca47d2aa7bd7ad6ed061430281bce
Aayushman Singh <aayushman2702@gmail.com>
2024-09-18 17:40:01 +0530

    test run

--- scraper/src/X/XTweets.ts
+++ scraper/src/X/XTweets.ts
@@ -1,8 +1,8 @@
-// InstagramPosts.ts
+// XPosts.ts
 import axios from 'axios';
 import { insertTweets } from '../mongoUtils.js';  // MongoDB utility function
 
-// Exported function for scraping Instagram posts
+// Exported function for scraping X posts
 export async function XTweets(startUrls: string[]) {
     const fullUrls = startUrls.map((username: string) => `https://www.x.com/${username}/`);
     const endpoint = 'https://api.apify.com/v2/acts/apidojo~tweet-scraper/run-sync-get-dataset-items?token=apify_api_PX0pmbuYEg3gO4cHjqIb8D8ah9MOnr2lJs5D';
@@ -20,9 +20,9 @@ export async function XTweets(startUrls: string[]) {
     };
 
     try {
-        console.log('Instagram scraper initiated for posts...');
+        console.log('X scraper initiated for posts...');
 
-        // Call the Apify Instagram scraper endpoint
+        // Call the Apify X scraper endpoint
         const response = await axios.post(endpoint, data, {
             headers: { 'Content-Type': 'application/json' }
         });
@@ -41,7 +41,7 @@ export async function XTweets(startUrls: string[]) {
 
         return { message: `Posts successfully inserted into collection for ${startUrls.join(', ')}` };
     } catch (error) {
-        console.error('Error scraping Instagram posts:', error.response ? error.response.data : error.message);
-        throw new Error('Error scraping Instagram posts');
+        console.error('Error scraping X posts:', error.response ? error.response.data : error.message);
+        throw new Error('Error scraping X posts');
     }
 }
