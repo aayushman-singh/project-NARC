73db304611e526ab6c16220329aaeedfaf4b26d1
Aayushman Singh <aayushman2702@gmail.com>
2024-11-20 20:25:05 +0530

    backend modulisation

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const searchHistorySchema = new mongoose.Schema({
     term: { type: String, required: true }, // The search term or URL
@@ -9,7 +9,7 @@ const searchHistorySchema = new mongoose.Schema({
     resultId: { type: mongoose.Schema.Types.ObjectId, ref: 'Result' }, // Reference to detailed result data
 });
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -20,17 +20,20 @@ const userSchema = mongoose.Schema({
     timestamps: true
 });
 
-userSchema.pre("save" , async function(next){
-    if(!this.isModified("password")){
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
         next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-})
+});
 
-userSchema.methods.matchPassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword , this.password)
-}
+// Method to compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default User;
