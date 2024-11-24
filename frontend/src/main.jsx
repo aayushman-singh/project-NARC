86687644b8c4d362ac5438c6e650da177ebb2312
arp1tk <arpitkukrety27@gmail.com>
2024-11-24 19:07:07 +0530

    maigret page update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
+    <SidebarProvider>
       <App />
+      </SidebarProvider>
     </Provider>
   </BrowserRouter>,
 );
