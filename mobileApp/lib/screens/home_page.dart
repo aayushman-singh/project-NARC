8d5fa99b37ae4912013c1df3394ad1d805547751
Aayushman Singh <aayushman2702@gmail.com>
2024-11-24 17:39:04 +0530

    auth routing

--- mobileApp/lib/screens/home_page.dart
+++ mobileApp/lib/screens/home_page.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../utils/routes.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
--- mobileApp/lib/screens/splash_screen_page.dart
+++ mobileApp/lib/screens/splash_screen_page.dart
@@ -1,23 +1,39 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tattletale/utils/routes.dart';
 
 class SplashScreenPage extends StatelessWidget {
   final bool showOnboarding;
 
-  const SplashScreenPage({Key? key, required this.showOnboarding})
-      : super(key: key);
+  Future<void> checkUserStatus(BuildContext context) async {
+    final storage = const FlutterSecureStorage();
+    final token = await storage.read(key: 'token');
+
+    // Check if onboarding has been completed
+    final prefs = await SharedPreferences.getInstance();
+    final showOnboarding = prefs.getBool('showOnboarding') ?? true;
+
+    // Navigate based on user status
+    if (token != null) {
+      navigateTo(context, Routes.home);
+    } else if (showOnboarding) {
+      navigateTo(context, Routes.onboarding);
+    } else {
+      navigateTo(context, Routes.auth);
+    }
+  }
+  const SplashScreenPage({super.key, required this.showOnboarding});
+
 
   @override
   Widget build(BuildContext context) {
     // Navigate after a delay
     Timer(const Duration(seconds: 2), () {
-      if (showOnboarding) {
-        navigateTo(context, Routes.onboarding);
-      } else {
-        navigateTo(context, Routes.home);
-      }
-    });
+      checkUserStatus(context);
+      });
+  
 
     return Scaffold(
       body: Center(
