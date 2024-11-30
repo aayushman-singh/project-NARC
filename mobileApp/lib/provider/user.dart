9f598579d670fa46755b4ee3f04d042d11de22cc
Aayushman Singh <aayushman2702@gmail.com>
2024-11-30 17:35:08 +0530

    added search history backend

--- mobileApp/lib/provider/user.dart
+++ mobileApp/lib/provider/user.dart
@@ -5,13 +5,20 @@ class UserProvider with ChangeNotifier {
   String? name;
   String? email;
   String? pic;
+  List<Map<String, dynamic>> searchHistory = [];
 
   // Update user data
-  void setUser({required String id, required String name, required String email, required String pic}) {
+  void setUser(
+      {required String id,
+      required String name,
+      required String email,
+      required String pic,
+      required List<Map<String,dynamic>> searchHistory}) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.pic = pic;
+    this.searchHistory = searchHistory;
     notifyListeners(); // Notify listeners about the state change
   }
 
@@ -21,6 +28,7 @@ class UserProvider with ChangeNotifier {
     name = null;
     email = null;
     pic = null;
+    searchHistory = [];
     notifyListeners();
   }
 }
